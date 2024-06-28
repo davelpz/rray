9d6ce6f6f5267bdd4377c5014cb277aa654825ee
David M. Lopez <davelpz@gmail.com>
2024-06-27 23:57:02 -0400

    bounding boxes kind of done

--- src/raytracer/object/group.rs
+++ src/raytracer/object/group.rs
@@ -1,18 +1,20 @@
-use std::sync::Arc;
+use std::sync::{Arc, RwLock, RwLockReadGuard};
+
 use crate::matrix::Matrix;
 use crate::raytracer::intersection::Intersection;
 use crate::raytracer::material::Material;
 use crate::raytracer::object::{AABB, Object};
+use crate::raytracer::object::{normal_to_world, world_to_object};
+use crate::raytracer::object::db::{add_object, get_next_id, get_object};
 use crate::raytracer::ray::Ray;
 use crate::tuple::Tuple;
-use crate::raytracer::object::db::{get_object, get_next_id, add_object};
-use crate::raytracer::object::{world_to_object, normal_to_world};
 
 pub struct Group {
     pub id: usize,
     pub parent_id: Option<usize>,
     pub transform: Matrix,
     pub child_ids: Vec<usize>,
+    aabb_cache: Arc<RwLock<Option<AABB>>>,  // Cache for the AABB wrapped in RwLock and Arc for thread safety
 }
 
 impl Group {
@@ -22,11 +24,28 @@ impl Group {
             parent_id: None,
             transform: Matrix::identity(4),
             child_ids: Vec::new(),
+            aabb_cache: Arc::new(RwLock::new(None)),  // Initialize the cache as None
         }
+    }
+
+    fn invalidate_aabb_cache(&self) {
+        // Invalidate the cache by acquiring a write lock and setting the value to None
+        let mut cache = self.aabb_cache.write().unwrap();
+        *cache = None;
+    }
+
+    fn get_aabb_cache(&self) -> RwLockReadGuard<Option<AABB>> {
+        self.aabb_cache.read().unwrap()
+    }
+
+    fn set_aabb_cache(&self, aabb: AABB) {
+        let mut cache = self.aabb_cache.write().unwrap();
+        *cache = Some(aabb);
     }
 
     pub fn add_child(&mut self, mut object: Arc<dyn Object + Send>) -> usize {
         Arc::get_mut(&mut object).unwrap().set_parent_id(self.id);
+        self.invalidate_aabb_cache();
         let child_id = object.get_id();
         add_object(object);
         self.child_ids.push(child_id);
@@ -35,12 +54,14 @@ impl Group {
 
     pub fn local_intersect(&self, ray: &Ray) -> Vec<Intersection> {
         let mut xs: Vec<Intersection> = Vec::new();
-        for child_id in &self.child_ids {
-            let child = get_object(*child_id);
-            let mut child_xs = child.intersect(ray);
-            xs.append(&mut child_xs);
+        if self.get_aabb().intersect(ray) {
+            for child_id in &self.child_ids {
+                let child = get_object(*child_id);
+                let mut child_xs = child.intersect(ray);
+                xs.append(&mut child_xs);
+            }
+            xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
         }
-        xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
         xs
     }
 
@@ -95,26 +116,46 @@ impl Object for Group {
     }
 
     fn get_aabb(&self) -> AABB {
-        todo!()
+        // Acquire a read lock and check if the cache is valid
+        if let Some(cached_aabb) = *self.get_aabb_cache() {
+            return cached_aabb;
+        }
+
+        let mut aabb: AABB = AABB::new(
+            Tuple::point(f64::INFINITY, f64::INFINITY, f64::INFINITY),
+            Tuple::point(f64::NEG_INFINITY, f64::NEG_INFINITY, f64::NEG_INFINITY),
+        );
+
+        for child_id in &self.child_ids {
+            let child = get_object(*child_id);
+            let child_aabb = child.get_aabb().apply_transform(child.get_transform());
+            aabb.adjust_aabb(&child_aabb);
+        }
+
+        // Cache the computed AABB
+        self.set_aabb_cache(aabb);
+
+        aabb
     }
 }
 
 #[cfg(test)]
 mod tests {
     use std::sync::Arc;
+
     use crate::color::Color;
     use crate::matrix::Matrix;
     use crate::raytracer::camera::Camera;
     use crate::raytracer::light::Light;
+    use crate::raytracer::object::{normal_to_world, world_to_object};
+    use crate::raytracer::object::cylinder::Cylinder;
+    use crate::raytracer::object::db::get_object;
     use crate::raytracer::object::group::Group;
     use crate::raytracer::object::Object;
     use crate::raytracer::object::sphere::Sphere;
     use crate::raytracer::ray::Ray;
     use crate::raytracer::scene::Scene;
     use crate::tuple::Tuple;
-    use crate::raytracer::object::{world_to_object, normal_to_world};
-    use crate::raytracer::object::cylinder::Cylinder;
-    use crate::raytracer::object::db::get_object;
 
     #[test]
     fn intersecting_a_ray_with_an_empty_group() {
